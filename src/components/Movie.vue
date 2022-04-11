755545c3eecdfb46224361b19ac7f1510935b3e4
unknown <chrispro.genius@gmail.com>
2022-04-11 07:48:03 +0300

    updated the arrangement

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -4,14 +4,14 @@
     <!--Main section starts-->
     <div class="d-md-flex container mx-lg-5">
         <div class="row mr-5">
-           <div v-for="movie in movies" :key="movie.title" class="col-md-3 mb-3">
-              <div v-if="movie.programType==='movie'" class="card">
+           <template v-for="movie in movies" :key="movie.title">
+              <div v-if="movie.programType==='movie'" class="card col-md-3 mb-3">
               <img class="card-img-top" :src="movie.images.posterArt.url" alt="Card image cap">
               <div class="card-body">
               <h5 class="card-title">{{movie.title}}</h5>
               </div>
               </div>
-           </div>
+           </template>
         </div>
     </div>
     <!--Main section ends-->
--- src/components/Series.vue
+++ src/components/Series.vue
@@ -4,14 +4,14 @@
     <!--Main section starts-->
     <div class="d-md-flex container mx-lg-5">
         <div class="row mr-5">
-           <div v-for="series in series" :key="series.title" class="col-md-3 mb-3">
-              <div v-if="series.programType==='series'" class="card">
+           <template v-for="series in series" :key="series.title">
+              <div v-if="series.programType==='series'" class="card col-md-3 mb-3">
               <img class="card-img-top" :src="series.images.posterArt.url" alt="Card image cap">
               <div class="card-body">
               <h5 class="card-title">{{series.title}}</h5>
               </div>
               </div>
-           </div>
+           </template>
         </div>
     </div>
     <!--Main section ends-->
